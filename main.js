63b251e1df3806465001baec6b91b6fea31b4015
Daniel Windsor <dw.accounts@protonmail.com>
2020-02-19 22:15:01 +1300

    Make functional calculations

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //Create event listeners
-
 const elements = document.getElementsByClassName('grid-item')
 
 for (let i = 0; i < elements.length; i++) {
@@ -7,7 +6,6 @@ for (let i = 0; i < elements.length; i++) {
 }
 
 //Process key presses
-
 let keyPress = []
 
 function log(evt) {
@@ -21,17 +19,60 @@ function log(evt) {
       keyPress.pop()
       break;
     case "=":
-      solve(keyPress)
+      sanitise(keyPress)
       break;
     default:
-      if (keyPress.length < 16) {
+      if (keyPress.length < 15) {
         keyPress.push(key)
       }
   }
-
   document.getElementsByClassName('grid-item-display')[0].innerHTML = keyPress.join('')
+
 }
 
-function solve(arr) {
+//Make sure equation won't break everything
+function sanitise(arr) {
 
+  //Make square root function
+  if (arr.includes("√")) {
+    let index = arr.indexOf("√")
+    arr[index] = "Math.sqrt("
+
+    let regex = /[+\-*\/]/
+
+    for (let i = index; i < arr.length; i++) {
+      console.log(i)
+      if(regex.test(arr[i])) {
+        arr.splice(i, 0, ")")
+        break;
+      } else if (i == arr.length - 1) {
+        arr.push(")")
+        break;
+      }
+    }
+
+  }
+
+  let equation = arr.join('').replace("x", "*")
+
+  //Make sure equation doesn't end with an operator
+  regex = /[^+\-*\/]$/
+  if (regex.test(equation)) {
+    solve(equation)
+  }
+}
+
+//Solve!
+function solve(equation) {
+
+  let solution = eval(equation)
+
+  //round solution if needed
+  if (solution.toString().length > 15) {
+    console.log("trigger")
+    solution = solution.toPrecision(12)
+  }
+
+  document.getElementsByClassName('grid-item-display')[0].innerHTML = solution
+  keyPress = [...solution.toString()]
 }
